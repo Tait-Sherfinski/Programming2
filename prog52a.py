60e2928d400e0c750dba207dec86c3e1187144a6
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-01-23 14:37:40 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,7 @@
+# Lang52a  area and periemter of a rectangle
+length = int(input("Enter length: "))
+width = int(input("Enter Width: "))
+area = length * width
+perim = 2 * length + 2 * width
+print("Area =", area)
+print("Perimeter = " + str(perim))
