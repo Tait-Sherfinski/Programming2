9a2d7fcb79e4a07e329d5a6f14e1ac169c594f20
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-01-23 14:30:30 +0000

    update

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Hello, World")
+
+
+""" Arithmetic operators
++ Add
+- Subtract
+* Multiply
+/ Divide
+// Floor divide(Divide and roundd down to nearest int)
+% Mod(Modulus/ remainder for int division)
+** Pow
+"""
+
+"""
+= Value Assignments (x = y)
++= Add to (x += y is shorthand for x = x + y)
+-= Subtract from(x -=y is shorthand for x = x - y)
+*=
+/=
+//=
+%=
+**=
+"""
+
+""" Data Types
+int - integer(whole number)
+float - Floating-Point number (number w/ a decimal)
+str - String (any text in quotes)
+bool - Boolean (either true or false)
+chr - Character (single keyboard character in single quotes)
