2ac7374e6fa701f58218422946623d435b15b1d0
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-01-23 14:45:47 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter Number 1: "))
+number2 = int(input("Enter Number 2: "))
+number3 = int(input("Enter Number 3: "))
+number4 = int(input("Enter Number 4: "))
+number5 = number1 + number2 + number3 + number4
+number6 = number5 / 4
+print("Sum =", number5)
+print("Average =", number6)
