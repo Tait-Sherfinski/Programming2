83bf3c471b453e1ece3b09a21f19f2f892333f9b
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-02-01 14:30:02 +0000

    update

---
+++ LP5-10.py
@@ -0,0 +1,6 @@
+import math
+
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Second Number: "))
+num3 = math.gcd(num1, num2)
+print("The Greatest Common Divisor is: " + str(num3))
