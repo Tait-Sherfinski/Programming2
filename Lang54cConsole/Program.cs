8b606c79f4358552d6b30f6df46f68fe8b087f4d
TaitSherfinski Sherfinski <Tsherfin@gmail.com>
2023-04-04 13:31:48 -0500

    update

---
+++ Lang54cConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lang54cConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius: ");
+            int radius = int.Parse(Console.ReadLine());
+            double circ = 2 * 3.14 * radius;
+            double area = 3.14 * (radius * radius);
+
+            Console.WriteLine("Circumference: " + circ);
+            Console.WriteLine("Area: " + area);
+            Console.ReadLine();
+        }
+    }
+}
