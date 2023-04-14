ab1668592e1123c4aa5f84689a2cc4722bfd1d4f
TaitSherfinski Sherfinski <Tsherfin@gmail.com>
2023-04-14 08:15:43 -0500

    update

---
+++ AboutMe/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "My name is Tait Sherfinski, My favorite sport is basketball, and my favorite quote is: 'If you can't fly then run, if you can't run then walk, if you can't walk then crawl, but whatever you do you have to keep moving forward'";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
