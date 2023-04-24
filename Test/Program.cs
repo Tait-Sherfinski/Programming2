fdfc8ac847dbce06e55ad077ef9d1fa271dd1462
TaitSherfinski Sherfinski <Tsherfin@gmail.com>
2023-04-24 08:23:41 -0500

    update

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, x;
+            a = 90;
+            b = 15;
+            c = 3;
+            x = a - b / 3 + c * 2 - 1;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
